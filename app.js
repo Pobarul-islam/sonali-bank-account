82ef6f077b93efe47b62827ae6140289023092e6
Pobarul-islam <pobarul3@gmail.com>
2022-02-17 15:49:51 +0600

    some code modified

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 function calculateBtn() {
   let incomeValue = document.getElementById("income-value");
   let mainBalance = document.getElementById("main-balance");
-    mainBalance.innerText = Number(mainBalance.innerText) + Number(incomeValue.value);
-    
-
-  
+  mainBalance.innerText = Number(mainBalance.innerText) + Number(incomeValue.value);
 
   let foodValue = document.getElementById("food-value");
   let rentValue = document.getElementById("rent-value");
-    let clothsValue = document.getElementById("cloths-value");
-    
-    let totalExpenses = document.getElementById("total-expenses");
-    
-    let totalProduct = Number(foodValue+rentValue+clothsValue);
+  let clothsValue = document.getElementById("cloths-value");
 
-    totalExpenses.value = Number(totalExpenses.value) + Number(totalProduct.innerText);
+  let totalProduct = Number(foodValue + rentValue + clothsValue);
 
+  let totalExpenses = document.getElementById("total-expenses");
 
-
-
+//   totalExpenses.value = Number(totalExpenses.value) + Number(totalProduct.innerText);
 
   totalExpenses.innerText = Number(totalExpenses.innerText) + Number(foodValue.value);
+  totalExpenses.innerText = Number(totalExpenses.innerText) + Number(rentValue.value);
+    totalExpenses.innerText = Number(totalExpenses.innerText) + Number(clothsValue.value);
+    foodValue.value = '';
+    rentValue.value = '';
+    clothsValue.value = '';
   incomeValue.value = "";
 }
 
